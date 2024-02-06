2162a3313b07f1746672241818cb4de42c9fb654
Tymofii Pashkevych <tymofii.pashkevych@gmail.com>
2024-02-06 23:43:13 +0200

    Done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,39 @@
+class Key {
+  private signature: number = Math.random();
+
+  getSignature(this: Key): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(this: Person): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected tenants: Person[] = [];
+
+  constructor(protected key: Key) {}
+
+  comeIn(this: House, person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+
+  openDoor(key: Key): void {}
+}
+class MyHouse extends House {
+  openDoor(this: MyHouse, key: Key): void {
+    this.door = this.key.getSignature() === key.getSignature();
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +42,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
